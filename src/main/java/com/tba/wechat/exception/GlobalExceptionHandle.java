c061fa58867a00b12f4a28ad3bb0540acb93e4a8
yangsenlin <1050696985@qq.com>
2023-03-29 09:39:57 +0800

    [feat]全局异常处理注解改为@RestControllerAdvice，统一返回@ResponseBody格式

--- src/main/java/com/tba/wechat/exception/GlobalExceptionHandle.java
+++ src/main/java/com/tba/wechat/exception/GlobalExceptionHandle.java
@@ -3,8 +3,12 @@ package com.tba.wechat.exception;
 import com.tba.wechat.web.domain.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * <p>
@@ -15,7 +19,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @since 2023-3-28 10:47
  */
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandle {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandle.class);
@@ -27,6 +31,26 @@ public class GlobalExceptionHandle {
             return Result.error(e.getMessage());
         }
         return Result.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public Result handlerNoFoundException(Exception e) {
+        LOGGER.error(e.getMessage(), e);
+        return Result.error(HttpStatus.NOT_FOUND.value(), "路径不存在，请检查路径是否正确");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result validExceptionHandler(MethodArgumentNotValidException e) {
+        LOGGER.error(e.getMessage(), e);
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        String message = fieldError == null ? "" : fieldError.getDefaultMessage();
+        return Result.error(message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e) {
+        LOGGER.error(e.getMessage(), e);
+        return Result.error(e.getMessage());
     }
 
 }
